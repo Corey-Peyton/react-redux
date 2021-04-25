65c6f0aae4fb7ba2d77f7aef1e414fa853ab3a8c
stevensam0109 <steven.sam.0109@outlook.com>
2021-04-25 16:21:23 +0530

    Api code added

--- src/using-api.js
+++ src/using-api.js
@@ -4,13 +4,13 @@ import axios from "axios";
 function UsingApi() {
   useEffect(() => {
     const api = axios
-      .get("https://api.fortnitetracker.com/v1/store")
-      .then((dat) => {
-        console.log(dat);
-      })
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((dat) => console.log(dat.data))
       .catch((err) => console.log(err));
+
     console.log(api);
   }, []);
+
   return <h1>Usinsdfds</h1>;
 }
 
