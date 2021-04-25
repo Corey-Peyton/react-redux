8db97e1a6b64dd25437222f5a06b01752364c32e
stevensam0109 <steven.sam.0109@outlook.com>
2021-04-25 16:09:21 +0530

    Api code added

--- src/using-api.js
+++ src/using-api.js
@@ -1,6 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function UsingApi() {
+  useEffect(() => {
+    const api = axios
+      .get("https://api.fortnitetracker.com/v1/store")
+      .then((dat) => {
+        console.log(dat);
+      })
+      .catch((err) => console.log(err));
+    console.log(api);
+  }, []);
   return <h1>Usinsdfds</h1>;
 }
 
