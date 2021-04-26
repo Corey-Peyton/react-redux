7500e5ca4479d7f45d858650c911037581602443
stevensam0109 <steven.sam.0109@outlook.com>
2021-04-26 11:09:15 +0530

    Update

--- src/details-api.js
+++ src/details-api.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 import { useParams, useHistory } from "react-router-dom";
 
 function DatailApi() {
+  const [api, setApi] = useState([]);
+
   const params = useParams();
   const history = useHistory();
   const currentState = params.state;
+
+  useEffect(() => {
+    axios
+      .get("https://api.covid19india.org/data.json")
+      .then((date) => {
+        setApi(date.data.statewise);
+        console.log(date.data.statewise);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <TopContainer>
       <Backbutton onClick={() => history.goBack()}>Back</Backbutton>
       <h1>{currentState}</h1>
+      {/* {api.map((arr) => {
+        return <h3>{arr.confirmed}</h3>;
+      })} */}
     </TopContainer>
   );
 }
